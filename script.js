5098262c51e94e0338e0dc431ed33df592b3d00c
niraj2024407 <neerajkumar04051998@gmail.com>
2025-08-31 09:46:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+window.onload = function () {
+  const colorSelect = document.getElementById("colorSelect");
+  const button = document.querySelector("input[type=button]");
+
+  button.addEventListener("click", function () {
+    const selectedIndex = colorSelect.selectedIndex;
+    if (selectedIndex !== -1) {
+      colorSelect.remove(selectedIndex);
+    } else {
+      alert("No color selected!");
+    }
+
+    // Optional: show message if no colors left
+    if (colorSelect.options.length === 0) {
+      alert("No colors left in the dropdown!");
+    }
+  });
+};
